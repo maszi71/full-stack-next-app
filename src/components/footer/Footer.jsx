e37c91197512b581c24a762bed39febab04a597b
masoudvrz <masoud.varzi@yahoo.com>
2024-02-27 18:59:48 +0330

    feat : implemented footer page

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,13 @@
-
-const Footer = ()=> {
-    return (<div>
-        <div>footer</div>
-       
-    </div>)
-}
-
+import styles from "./footer.module.css";
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>Lamadev</div>
+      <div className={styles.text}>
+        Lama Creative thoughts agency All rights reserved
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
