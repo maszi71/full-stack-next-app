b4e1045018a0d52eed3efffc782514660839a0b5
masoudvrz <masoud.varzi@yahoo.com>
2024-02-27 18:59:04 +0330

    feat : updated about us page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,12 +1,40 @@
 import Image from "next/image";
 import styles from "./about.module.css";
+import aboutImg from "../../../public/images/about.png";
 
+const AboutUs = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h1 className={styles.title}>
+          We create digital ideas that are bigger , bolder , braver and better
+        </h1>
+        <p className={styles.desc}>
+          We create ideas that are bigger , bolder , braver and better.we
+          believe in good ideas and flexibility{" "}
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10k+</h1>
+            <p>Year of exprience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10k+</h1>
+            <p>Year of exprience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10k+</h1>
+            <p>Year of exprience</p>
+          </div>
+        </div>
+      </div>
 
-  const AboutUs = ()=> {
-    
-    return <div className={styles.container}>
-      <Image src="./about.png" alt="about us" fill />
+      <div className={styles.imgContainer}>
+        <Image className={styles.img} src={aboutImg} alt="about us" fill />
+      </div>
     </div>
-  }
+  );
+};
 
-  export default AboutUs;
+export default AboutUs;
