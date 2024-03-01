dce20219607597023652e48b54ce520b0d8f18f8
masoudvrz <masoud.varzi@yahoo.com>
2024-03-01 16:16:57 +0330

    feat : updated contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,14 +1,23 @@
 import styles from "./contact.module.css"
 import Image from "next/image";
 import contactImg from "../../../public/images/contact.png";
+// import HydrationTest from "@/components/hydrationTest";
+// const HydrationTestNoSSR = dynamic(()=>import("@/components/hydrationTest"), {ssr: false})
 
 const ContactUs = () => {
+
+  // random number is gonna different in server and in client side even the component use "use client"
+   // const a = Math.random();
+  // console.log(a);
+
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
         <Image src={contactImg} alt="Contact us" fill className={styles.img} />
       </div>
       <div className={styles.formContainer}>
+         {/* <HydrationTestNoSSR/> */}
+        {/* <div suppressHydrationWarning>{a}</div> */}
         <form action="" className={styles.form}>
           <input type="text" placeholder="Name and Surname" />
           <input type="text" placeholder="Email Address" />
