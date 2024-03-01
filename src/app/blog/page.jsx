6c48ec46a8f964848a5d3e30374783a6330191c8
masoudvrz <masoud.varzi@yahoo.com>
2024-03-01 19:02:14 +0330

    feat : fetch blog data

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,20 +1,21 @@
 import PostCard from "../../components/postCard/PostCard";
 import styles from "./blog.module.css";
+import {getPosts} from "../../../lib/data"
+// const getData = async () => {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+//     next: { revalidate: 3600 },
+//   });
 
-const getData = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
-    next: { revalidate: 3600 },
-  });
+//   if (!res.ok) {
+//     throw new Error("Something went wrong");
+//   }
 
-  if (!res.ok) {
-    throw new Error("Something went wrong");
-  }
-
-  return res.json();
-};
+//   return res.json();
+// };
 
 const Blog = async () => {
-  const posts = await getData();
+ // const posts = await getData();
+  const posts = await getPosts();
 
   return (
     <div className={styles.container}>
