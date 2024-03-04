0b3f2b746782f32f5053ddf76af1e4c1d30fd9f4
masoudvrz <masoud.varzi@yahoo.com>
2024-03-04 13:01:54 +0330

    feat : fetched blog post from DB

--- src/components/postUser/postUser.jsx
+++ src/components/postUser/postUser.jsx
@@ -1,31 +1,28 @@
 import styles from "./postUser.module.css";
 import Image from "next/image";
 import noAvatar from "../../../public/images/noavatar.png";
+import { getUser } from "../../../lib/data";
 
 // FETCH DATA WITH AN API
-const getData = async (userId) => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}` ,{cache:"no-store"});
+// const getData = async (userId) => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}` ,{cache:"no-store"});
 
-  if (!res.ok) {
-    throw new Error("Something went wrong");
-  }
+//   if (!res.ok) {
+//     throw new Error("Something went wrong");
+//   }
 
-  return res.json();
-};
+//   return res.json();
+// };
 
 const PostUser = async ({ userId }) => {
-  // FETCH DATA WITH AN API
-   const user = await getData(userId);
-
-  // FETCH DATA WITHOUT AN API
- // const user = await getUser(userId);
+  const user = await getUser(userId);
 
   return (
     <div className={styles.container}>
       <Image
         className={styles.avatar}
         src={user.img ? user.img : noAvatar}
-        alt=""
+        alt="user image"
         width={50}
         height={50}
       />
